c672e77fb22bd8617d93c59511ca698e581812e7
Andy Roshadi <andyroshadi@192.168.1.5>
2021-03-01 01:21:43 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App Vol. 2"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
